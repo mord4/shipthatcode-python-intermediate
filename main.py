49fc629cdd1071aa36f1ef57bbe5e871ef627e93
mord4 <fedorsmolin2006@gmail.com>
2026-07-30 17:07:43 +0300

    done

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from collections import defaultdict
+a = set(input().strip().split())
+b = set(input().strip().split())
 
-n = int(input())
-a = [input().strip().split() for _ in range(n)]
-
-d = defaultdict(list)
-for key, value in a:
-    d[value].append(key)
-
-for k, v in d.items():
-    print(k + ":", ", ".join(v))
+print(" ".join(sorted(list(a & b))))
