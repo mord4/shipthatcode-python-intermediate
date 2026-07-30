9a07ca6707f9fe8e35a4ec1a3e3fd0ed199ea6fa
mord4 <fedorsmolin2006@gmail.com>
2026-07-30 16:21:46 +0300

    done

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 n = int(input())
 nums = [int(input()) for _ in range(n)]
-# Find and print the second largest unique value
+
+uniqs = set(nums)
+
+first = nums[0]
+first_count = nums.count(first)
+second = 0
+second_count = 100000
+
+for i in uniqs:
+    if nums.count(i) < first_count:
+        second = first
+        second_count = first_count
+        first = i
+        first_count = nums.count(i)
+
+    elif nums.count(i) == first_count and i > first:
+        second = first
+        second_count = first_count
+        first = i
+        first_count = nums.count(i)
+
+    elif nums.count(i) < second_count:
+        second = i
+        second_count = nums.count(i)
+
+    elif nums.count(i) == second_count and i > second:
+        second = i
+        second_count = nums.count(i)
+
+
+print(second)
